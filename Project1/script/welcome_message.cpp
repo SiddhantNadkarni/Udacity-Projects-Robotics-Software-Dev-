b77fcbaec3ec6aa50911b86df45bbc916324d971
root <siddhant.snadkarni@gmail.com>
2019-06-19 02:05:21 +0000

    Plugin edits

--- Project1/script/welcome_message.cpp
+++ Project1/script/welcome_message.cpp
@@ -1,21 +1,22 @@
 #include <gazebo/gazebo.hh>
 
+
 namespace gazebo
 {
 	class WorldPluginMyRobot: public WorldPlugin
 	{
 	public:
-		WorldPluginMyRobot()
+		WorldPluginMyRobot(): WorldPlugin()
 		{
-			printf("Hello world\n");
+			printf("Hello World\n");
 		}
+
 	public:
-		void Load(physics::WorldPtr_world, sdf::ElementPtr_sdf)
+		void Load(physics::WorldPtr _world, sdf::ElementPtr _sdf)
 		{
 
 		}
-
-		
+				
 	};
 
 	GZ_REGISTER_WORLD_PLUGIN(WorldPluginMyRobot)
